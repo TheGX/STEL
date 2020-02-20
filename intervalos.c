0e79094f745060229b1e3f024554713eb00778ab
TheGX <goncalox11@gmail.com>
2020-02-20 19:05:26 +0000

    House keeping

--- intervalos.c
+++ intervalos.c
@@ -22,11 +22,9 @@ int main(int argc, char const *argv[]) {
     srand ( time ( NULL));
     double long u = (double) rand()/RAND_MAX;
     
-    // double c = log(u);
     double c = -(1-lambda)*log(u);
-    // printf("lambda = %d\n n_samples = %d\n u = %Lf\n c = %Lf\n", lambda, n_samples, u, c);
-    printf("lambda = %lf\n n_samples = %d\n u = %Lf\n c = %lf\n", lambda, n_samples, u, c);
 
+    printf("lambda = %lf\n n_samples = %d\n u = %Lf\n c = %lf\n", lambda, n_samples, u, c);
 
     return 1;
 }
