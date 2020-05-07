5a965f2d741a94ab8d6128115c54de76a217fe9c
TheGX <goncalox11@gmail.com>
2020-05-08 00:52:29 +0100

    Added erros and trust intervals

--- CallCenter.c
+++ CallCenter.c
@@ -5,7 +5,7 @@
 #include"prototipo.c"
 #include"printGraph.c"
 
-#define	lambda 0.022f
+#define	lambda 0.0347f
 #define	dm 120.0f
 #define	ARRIVAL 0
 #define	DEPARTURE 1
@@ -16,7 +16,7 @@
 
 /*return random number between zero and one */
 double generate_random();
-
+double media(double* values, int n);
 /*returns if a call is general or general+specific*/
 int get_call_area();
 
@@ -28,6 +28,7 @@ double duration_of_call_specific();
 
 /*returns time between calls*/
 double time_between_calls();
+double stddev(double* values, int n, double average);
 
 /*returns running average delay*/
 double running_average(int n, double current_sample, double previous_avg);
@@ -53,35 +54,39 @@ int main(int argc, char const *argv[]) {
 	
     srand(time(0));
     int size = (max_delta/delta);
-    int delayed=0, general_bussy=0, specific_bussy=0, area = 0, aux_n_specific=0;
-	int n_specific = 0, specific_delayed=0;
-    double d=0, c = 0, total_delay=0, total_specific_delay=0; 
-    double delay=0, specific_delay=0; 
-	double avg=0, i_delay=0;
+    int delayed=0, general_bussy=0, specific_bussy=0, area = 0;
+	int n_specific = 0, specific_delayed=0, error_count=0;
+    double d=0, total_delay=0, total_specific_delay=0, rel_error_sum=0, abs_error_sum=0; 
+    double delay=0, specific_delay=0, avg_specific_total_delay=0; 
+	double avg=0, abs_error=0, rel_error=0;
+	Array Array_delays;
+	initArray(&Array_delays,10);
     int *general_histograma = (int *)malloc(size*sizeof(int));
     int *prediction_histograma = (int *)malloc(size*sizeof(int));
     int i=0; 
     while(i < n_samples) {
-    	if(i == 0) system = adicionar(system, ARRIVAL, SPECIFIC, 0);
+    	if(i == 0) system = adicionar(system, ARRIVAL, SPECIFIC, 0, 0);
 		
 		if(system ->tipo == ARRIVAL){
 			//GENERATE NEXT ARRIVAL
 			area = get_call_area();
 			i++;
 			d = time_between_calls();
-			system = adicionar(system, ARRIVAL, area, (system->tempo + d));
+			system = adicionar(system, ARRIVAL, area, 0, (system->tempo + d));
 			
 			//PROCESS EVENT THAT ARRIVED
 			if(general_bussy < n_general_channels) {
 				//GENERAL RESOURCES AVAILABLE 
 				general_bussy++;
 				d = duration_of_call_general(system->area); 
-				system = adicionar(system, DEPARTURE, system->area, (system->tempo + d));
+				system = adicionar(system, DEPARTURE, system->area, 0, (system->tempo + d));
 			}else if(lenght < capacity) {
 				//QUEUE EVENT
-				general_queue = adicionar(general_queue, ARRIVAL, system->area, system->tempo);
+				general_queue = adicionar(general_queue, ARRIVAL, system->area, 0, system->tempo);
 				lenght++;
 				delayed++;
+				// avg = running_average(delayed, delay, avg);
+				// printf("Sample %d - Running Avg: %lf\n", i, avg);
 			} else blocked++; //QUEUE FULL -> BLOCKED
 		}
 		else { 	//GENERAL DEPARTURE
@@ -90,27 +95,32 @@ int main(int argc, char const *argv[]) {
 			if(general_queue != NULL && capacity > 0) {
 				general_bussy++;
 				delay = system->tempo - general_queue->tempo;
+				general_queue->suffered_delay +=delay;
 				total_delay += delay;
 
 				//ADD PREVIOUS (i-1) PREDICTION ERROR
-				prediction_histograma = histogram(fabs(avg-delay), size, prediction_histograma, delta);
+				abs_error = fabs(delay-avg);
+				error_count++;
+				rel_error_sum += rel_error;
+				abs_error_sum += abs_error;
+				prediction_histograma = histogram(abs_error, size, prediction_histograma, delta);
 				//COMPUTE RUNNING AVG FOR CURRENT I
-				printf("Sample %d - Running Avg: %lf\n", i, avg);
-				avg = running_average(i, delay, avg);
+				avg = running_average(error_count, delay, avg);
 
 				general_histograma = histogram(delay, size, general_histograma, delta);
 				d = duration_of_call_general(general_queue->area); 
-				system = adicionar(system, DEPARTURE, general_queue->area, (system->tempo + d));
+				system = adicionar(system, DEPARTURE, general_queue->area, general_queue->suffered_delay, (system->tempo + d));
 				general_queue = remover(general_queue);
 				lenght--;
 			}
 			
 			if(system->area == SPECIFIC) {
+				int i =n_specific;
 				n_specific++;
 				//PROCESS EVENT THAT ARRIVED
 
-				specific = adicionar(specific, ARRIVAL, SPECIFIC, system->tempo);
-				while(specific->tempo < system->tempo) {
+				specific = adicionar(specific, ARRIVAL, SPECIFIC, system->suffered_delay, system->tempo);
+				while(specific->tempo < system->tempo && n_specific_channels > 0 && specific != NULL) {
 
 					if(specific->tipo == DEPARTURE){
 						specific_bussy--;
@@ -119,20 +129,30 @@ int main(int argc, char const *argv[]) {
 						if(specific_queue != NULL){
 							specific_bussy++;
 							specific_delay = (specific->tempo - specific_queue->tempo);
+							specific_queue->suffered_delay += specific_delay;
 							total_specific_delay += specific_delay;
+
 							d = duration_of_call_specific();
+							// test[i] = specific->suffered_delay; 
+							i++;
+							avg_specific_total_delay+= specific_queue->suffered_delay; 
+							insertArray(&Array_delays, specific_queue->suffered_delay);
+							specific = adicionar(specific, DEPARTURE, SPECIFIC, specific_queue->suffered_delay, (specific->tempo + d));
 							specific_queue = remover(specific_queue); 
-							specific = adicionar(specific, DEPARTURE, SPECIFIC, (specific->tempo + d));
 						}
 					} else {
 						if(specific_bussy < n_specific_channels) {
 							//SPECIFIC RESOURCES AVAILABLE
 							specific_bussy++;
-							d = duration_of_call_specific(); 
-							specific = adicionar(specific, DEPARTURE, SPECIFIC, (specific->tempo + d));
+							d = duration_of_call_specific();
+							// test[i] = specific->suffered_delay; 
+							i++;
+							avg_specific_total_delay+= specific->suffered_delay;
+							insertArray(&Array_delays, specific->suffered_delay);
+							specific = adicionar(specific, DEPARTURE, SPECIFIC, specific->suffered_delay, (specific->tempo + d));
 						} else {
 							//QUEUE SPECIFIC EVENT
-							specific_queue = adicionar(specific_queue, ARRIVAL, SPECIFIC, specific->tempo);
+							specific_queue = adicionar(specific_queue, ARRIVAL, SPECIFIC, specific->suffered_delay, specific->tempo);
 							specific_delayed++;
 						}
 					}
@@ -144,18 +164,34 @@ int main(int argc, char const *argv[]) {
 		system = remover(system);
     }	
 	// printGraph(prediction_histograma,25);
+	double avg_delay = (double)total_delay/delayed;
     printf("Avg Packets Blocked:\t %lf%%\n",(double)blocked/n_samples *100);
     printf("Avg Packets Delayed:\t %lf%%\n",(double)delayed/n_samples *100);
-    printf("Avg Delayed Time:\t %lf\n",(double)total_delay/delayed);
+    printf("Avg Delayed Time:\t %lf\n",avg_delay);
     printf("Avg Spec Delayed:\t %lf%%\n",(double)specific_delayed/n_specific *100);
     printf("Avg Spec Delayed Time:\t %lf\n", (double)total_specific_delay/specific_delayed);
+	double avg_queue_time_spec = media(Array_delays.array, Array_delays.used); 
+    printf("Avg Time Spec Packets in Queues:\t %lf\n", avg_queue_time_spec);
+	
+	double avg_abs =  (double)abs_error_sum/error_count;
+    printf("\nAvg Abs Prediction Error:\t %lf\n", avg_abs);
+	rel_error = fabs(abs_error/avg_delay);
+    printf("Avg Rel Prediction Error:\t %lf%%\n", rel_error *100);
+	
 
-	FILE *f =fopen( "Delay.txt", "a");
-	for (int i = 0; i < 25; i++)
-	{
-		fprintf(f, "%d\n", general_histograma[i]);
-	}
-	fclose(f);
+	double desv_padrao = stddev(Array_delays.array , Array_delays.used, avg_queue_time_spec);
+	double erro_padrao_media = desv_padrao/(sqrt(Array_delays.used));
+
+	double limite_confi = 1.65;
+	double interv_confi = limite_confi*erro_padrao_media;
+	printf("\nIntervalo confiança, com limite 90%% %lf\n", interv_confi);
+
+	// FILE *f =fopen( "Delay.txt", "a");
+	// for (int i = 0; i < 25; i++)
+	// {
+	// 	fprintf(f, "%d\n", prediction_histograma[i]);
+	// }
+	// fclose(f);
     return 1;
 }
 
@@ -204,5 +240,24 @@ double duration_of_call_specific(){
 }
 
 double running_average(int n, double current_sample, double previous_avg){
-		return (previous_avg*(n-1))/n + current_sample/n;
+		return (previous_avg*(n-1) + current_sample)/n;
+}
+
+double media(double* values, int n)
+{
+    int i;
+    double s = 0;
+ 
+    for ( i = 0; i < n; i++ )
+        s += values[i];
+    return s / n;
+}
+double stddev(double* values, int n, double average)
+{
+    int i;
+    double s = 0;
+ 
+    for ( i = 0; i < n; i++ )
+        s += (values[i] - average) * (values[i] - average);
+    return sqrt(s / (n - 1));
 }
--- prototipo.c
+++ prototipo.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+typedef struct {
+  double *array;
+  size_t used;
+  size_t size;
+} Array;
+
 // Definição da estrutura da lista
 typedef struct{
 	int tipo;
 	int area;
+	double suffered_delay;
 	double tempo;
 	struct lista * proximo;
 } lista;
@@ -19,7 +27,7 @@ lista * remover (lista * apontador)
 }
 
 // Função que adiciona novo elemento à lista, ordenando a mesma por tempo
-lista * adicionar (lista * apontador, int n_tipo, int area, double n_tempo)
+lista * adicionar (lista * apontador, int n_tipo, int area, double suffered_delay, double n_tempo)
 {
 	lista * lap = apontador;
 	lista * ap_aux, * ap_next;
@@ -29,6 +37,7 @@ lista * adicionar (lista * apontador, int n_tipo, int area, double n_tempo)
 		apontador -> proximo = NULL;
 		apontador -> tipo = n_tipo;
 		apontador -> area = area;
+		apontador -> suffered_delay = suffered_delay;
 		apontador -> tempo = n_tempo;
 		return apontador;
 	}
@@ -38,6 +47,7 @@ lista * adicionar (lista * apontador, int n_tipo, int area, double n_tempo)
 	        ap_aux = (lista *) malloc(sizeof (lista));
 	        ap_aux -> tipo = n_tipo;
 	        ap_aux -> area = area;
+			ap_aux -> suffered_delay = suffered_delay;
             ap_aux -> tempo = n_tempo;
             ap_aux -> proximo = (struct lista *) apontador;
             return ap_aux;
@@ -60,6 +70,7 @@ lista * adicionar (lista * apontador, int n_tipo, int area, double n_tempo)
 			apontador -> proximo = NULL;
 		apontador -> tipo = n_tipo;
 		apontador -> area = area;
+		apontador -> suffered_delay = suffered_delay;
 		apontador -> tempo = n_tempo;
 		return lap;
 	}
@@ -78,4 +89,27 @@ void imprimir (lista * apontador)
 			apontador = (lista *)apontador -> proximo;
 		}
 	}
+}
+
+
+void initArray(Array *a, size_t initialSize) {
+  a->array = (double *)malloc(initialSize * sizeof(double));
+  a->used = 0;
+  a->size = initialSize;
+}
+
+void insertArray(Array *a, int element) {
+  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+  // Therefore a->used can go up to a->size 
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = (double *)realloc(a->array, a->size * sizeof(double));
+  }
+  a->array[a->used++] = element;
+}
+
+void freeArray(Array *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
 }
